38ccfc305054ef73bdd080a1e5a51fc2caaa5912
Dhaval.Thanki <dhaval.thanki07@gmail.com>
2022-08-30 19:08:54 +0530

    first
    first commit

---
+++ MyApp/src/test/java/com/dhaval/myapp/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhaval.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
